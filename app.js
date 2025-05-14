941b3be01d13928db28ff4079cf5e31ba017e581
JiannyAyala <jiannyjj@gmail.com>
2025-05-14 16:24:09 -0400

    needed help with GET /employees/random so i got it from the solution, thanks!

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ app.route("employees").get((req, res) => {
   res.send(employees);
 });
 
+app.route("/employees/random").get((req, res) => {
+  const randomIndex = Math.floor(Math.random() * employees.length);
+  res.send(employees[randomIndex]);
+});
+
 app.route("employees/:id").get((req, res) => {
   const { id } = req.params;
   const employee = employees[id];
   if (!employee) return res.status(404).send("Wrong EMPLOYEE");
   res.send(employee);
 });
+
+app.route("/employees/random").get((req, res) => {});
