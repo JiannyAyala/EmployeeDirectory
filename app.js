a796779d638ec414456be06343de5544c4f8718d
JiannyAyala <jiannyjj@gmail.com>
2025-05-14 16:20:49 -0400

    GET /employees/:id sends employee with id or a 404 if no employee exist

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ app.route("/").get((req, res) => {
 app.route("employees").get((req, res) => {
   res.send(employees);
 });
+
+app.route("employees/:id").get((req, res) => {
+  const { id } = req.params;
+  const employee = employees[id];
+  if (!employee) return res.status(404).send("Wrong EMPLOYEE");
+  res.send(employee);
+});
